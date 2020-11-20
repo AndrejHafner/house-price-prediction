5425f5b6797b96deaca237ba37de088de19f9047
Andrej Hafner <andrej.hafn3r@gmail.com>
2020-11-20 19:29:18 +0100

    First test submission

---
+++ predict.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+
+# Load data
+df_train = pd.read_csv("data/train.csv")
+df_test = pd.read_csv("data/test.csv")
+
+
+
+
