Fixed log normalization error for YearBuilt feature

main.py
@@ -1,12 +1,13 @@
+import warnings
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from scipy.stats import skew, boxcox_normmax, probplot
+from scipy.stats import skew, boxcox_normmax, probplot, PearsonRConstantInputWarning, PearsonRNearConstantInputWarning
 from scipy.special import boxcox1p
-from sklearn.preprocessing import StandardScaler
 from models import train, save_submission, cv_all_models, cv, get_model
+
 
 
 def handle_missing_data(df):
@@ -45,13 +46,19 @@ def handle_missing_data(df):
             objects.append(i)
     df.update(df[objects].fillna('None'))
 
-    # And we do the same thing for numerical features, but this time with 0s
+    # Same thing for numerical features, but this time with 0s
     numeric_dtypes = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     numeric = []
     for i in df.columns:
         if df[i].dtype in numeric_dtypes:
             numeric.append(i)
     df.update(df[numeric].fillna(0))
+
+    # Some of the non-numeric predictors are stored as numbers -  we convert them into strings
+    df['MSSubClass'] = df['MSSubClass'].apply(str)
+    df['YrSold'] = df['YrSold'].astype(str)
+    df['MoSold'] = df['MoSold'].astype(str)
+
     return df
 
 def fix_skewness(df, skew_thresh = 0.5):
@@ -66,8 +73,8 @@ def fix_skewness(df, skew_thresh = 0.5):
     # Calculate the skew for all the numeric features
     features_skew = df[numeric].apply(lambda x: skew(x)).sort_values(ascending=False)
 
-    # Define all the features that have absolute skewness above 0.5 as skewed and apply transformation
-    high_skew = features_skew[features_skew.abs() > skew_thresh]
+    # Define all the features that have skewness above 0.5 as skewed and apply transformation
+    high_skew = features_skew[features_skew > skew_thresh]
 
     # Apply Box-Cox transformation to skewed features
     for i in high_skew.index:
@@ -83,10 +90,6 @@ def remove_outliars(df):
 def clean_data(df):
     df = handle_missing_data(df)
     df = remove_outliars(df)
-
-    # Remove some of the features - should we? TODO
-    # df = df.drop(['Street' ], axis=1)
-
     return df
 
 def feature_engineering(df):
@@ -138,22 +141,8 @@ def preprocess_df(df_train, df_test):
     train_x = df[df["train_set"] == True]
     test_x = df[df["train_set"] == False]
 
-
-    # train_x =
     return train_x, train_y, test_x
 
-def preprocess_df_test(df_test, removed_features):
-    df_test = df_test.drop(removed_features, 1)
-    df_test = fix_skewness(df_test)
-    df_test = feature_engineering(df_test)
-
-    # Convert categorical data into dummy variables - one hot encoding
-    df_test = pd.get_dummies(df_test)
-
-    # Remove ID - helps nothing as it uniquely identify every entry
-    df_test = df_test.drop(['Id'], axis=1)
-
-    return df_test
 
 def main():
     df_train = pd.read_csv("data/train.csv")
@@ -161,9 +150,8 @@ def main():
 
     train_x, train_y, test_x = preprocess_df(df_train, df_test)
 
-
     cv_all_models(train_x, train_y)
-
+    # cv(get_model("rf"), train_x, train_y)
     # Train models
     # model = train("xgb", train_x, train_y)
     #
@@ -178,4 +166,6 @@ def main():
 
 
 if __name__ == '__main__':
+    warnings.filterwarnings("ignore", category=PearsonRConstantInputWarning)
+    warnings.filterwarnings("ignore", category=PearsonRNearConstantInputWarning)
     main()

models.py
@@ -15,7 +15,7 @@ from utils import *
 from config import *
 
 
-models = ["svm","rf","xgb","lgbm", "lr", "avg"]
+models = ["svm", "xgb","lgbm", "lr", "avg", "rf"]
 
 def train(model, x, y, print_progress = True, **kwargs):
 
@@ -44,41 +44,41 @@ def save_submission(model, predictions, df_test):
     elif isinstance(model, DummyRegressor):
         save_submission_file(DUMMY_REGRESSOR_AVG_SUBMISSION_DIR, df_test, predictions)
 
-def cv(model, x, y, k = 10):
+def cv(model, x, y, k = 10, model_name = ""):
     scorers = {
         "rmse": make_scorer(mean_squared_error, squared=False)
     }
 
-    cv_result = cross_validate(model, x, y, cv=k, n_jobs=8, scoring=scorers)
+    cv_result = cross_validate(model, x, y.values.ravel(), cv=k, n_jobs=8, scoring=scorers)
     rmse = float(np.mean(cv_result["test_rmse"]))
-    print(f"Cross validation complete. Folds: { k }, Mean RMSE: { round(rmse, 3) }.")
+    print(f"Cross validation complete - { model_name }. Folds: { k }, Mean RMSE: { round(rmse, 3) }.")
 
 
 def cv_all_models(x,y, **kwargs):
     for model_name in models:
-        cv(get_model(model_name), x, y, **kwargs)
+        cv(get_model(model_name), x, y, **kwargs, model_name=model_name)
 
 def get_model(model, **kwargs):
     if model not in models:
         print("Unknown model - exiting.")
         return
 
-    if model is "svm":
+    if model == "svm":
         if len(kwargs) == 0: kwargs = SVM_CONFIG
         return make_pipeline(RobustScaler(), SVR(**kwargs))
-    elif model is "rf":
+    elif model == "rf":
         if len(kwargs) == 0: kwargs = RANDOM_FOREST_CONFIG
         return RandomForestRegressor(**kwargs)
-    elif model is "xgb":
+    elif model == "xgb":
         if len(kwargs) == 0: kwargs = XGBOOST_CONFIG
         return xgb.XGBRegressor(**kwargs)
-    elif model is "lgbm":
+    elif model == "lgbm":
         if len(kwargs) == 0: kwargs = LIGHTGBM_CONFIG
         return lgbm.LGBMRegressor(**kwargs)
-    elif model is "lr":
+    elif model == "lr":
         if len(kwargs) == 0: kwargs = LR_CONFIG
         return LinearRegression(**kwargs)
-    elif model is "avg":
+    elif model == "avg":
         if len(kwargs) == 0: kwargs = AVG_CONFIG
         return DummyRegressor(**kwargs)
 